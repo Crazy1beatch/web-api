5b138a1a0d23e7c503aff9e6bad38d611715228f
Crazy1beatch <zhora.shevelev196@mail.ru>
2024-02-29 19:41:31 +0500

    Первая таска

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,22 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
 namespace WebApi.MinimalApi.Controllers;
 
+[Produces("application/json", "application/xml")]
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : Controller
 {
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
+    
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        this.mapper = mapper;
+        this.userRepository = userRepository;
     }
 
     [HttpGet("{userId}")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = userRepository.FindById(userId);
+        if (user is null)
+            return NotFound();
+        
+        return Ok(mapper.Map<UserDto>(user));
     }
 
     [HttpPost]
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,7 +1,39 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("https://localhost:5001;http://localhost:5000");
-builder.WebHost.UseEnvironment("Development");
+//builder.WebHost.UseEnvironment("Development");
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddControllers(options =>
+    {
+        // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+        options.ReturnHttpNotAcceptable = true;
+        // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+        // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+        options.RespectBrowserAcceptHeader = true;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+        options.SuppressMapClientErrors = true;
+    });
+
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.Id, opt => opt.MapFrom(c => c.Id))
+        .ForMember(dest => dest.FullName, opt => opt.MapFrom(c => $"{c.LastName} {c.FirstName}"))
+        .ForMember(dest => dest.Login, opt => opt.MapFrom(c => c.Login))
+        .ForMember(dest => dest.GamesPlayed, opt => opt.MapFrom(c => c.GamesPlayed))
+        .ForMember(dest => dest.CurrentGameId, opt => opt.MapFrom(c => c.CurrentGameId));
+});
 
 var app = builder.Build();
 
